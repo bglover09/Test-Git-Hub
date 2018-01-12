51eee30172b14f30cdf12cdc3ead4411f26f9dd1
Brady Glover <bradyg009@gmail.com>
2018-01-12 16:37:33 -0600

    Add asset folder

---
+++ Assets/MovingTarget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovingTarget : MonoBehaviour {
+
+    float speed = .8f;
+    private float zMax = 4.5f;
+    private float zMin = -4.5f;
+    private int dir = 1;
+	private float radius = 4;
+	private float theta = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		theta = theta + speed * Time.deltaTime;
+		theta = theta % 360;
+		var newX = Mathf.Cos(theta ) * radius;
+		var newZ = Mathf.Sin (theta ) * radius;
+        transform.position = new Vector3(newX, .75f, newZ);
+		
+	}
+}
---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementScript : MonoBehaviour {
+    private Rigidbody rigidbody;
+    private Collider collider;
+    
+    public GameObject[] Collidables;
+    int winCount;
+
+    public Canvas canvas;
+    int count = 0;
+
+	void Start () {
+        winCount = Collidables.Length;
+        this.transform.position = new Vector3(0,0.5f,0);
+
+        rigidbody = this.transform.GetComponent<Rigidbody>();
+        collider = this.transform.GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup")) { other.gameObject.SetActive(false); count++;
+            UpdatePoints();
+            if(count >= winCount)
+            {
+                DisaplayWinMessage();
+            }
+        }
+        
+    }
+    private void DisaplayWinMessage()
+    {
+        var text = canvas.transform.Find("WinText").GetComponent<Text>();
+        text.text = "Congratulations, you are to win!";
+    }
+	private void UpdatePoints()
+    {
+        var text = canvas.transform.Find("PointValues").GetComponent<Text>();
+        text.text = count.ToString();
+              
+    }
+    private void ResetGame()
+    {
+        Debug.Log("Reseting board");
+        this.transform.position = new Vector3(0, 0.5f, 0);
+        count = 0;
+        var text = canvas.transform.Find("WinText").GetComponent<Text>();
+        text.text = "";
+
+        text = canvas.transform.Find("PointValues").GetComponent<Text>();
+        text.text = "";
+
+        Collidables.ToList().ForEach(x=>x.gameObject.SetActive(true));
+
+    }
+
+	void FixedUpdate () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResetGame();
+        }
+
+
+        var forceDirection = new Vector3(0, 0, 0);
+        if(Input.GetKey(KeyCode.DownArrow)){
+            forceDirection += Vector3.back;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            forceDirection += Vector3.forward;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            forceDirection += Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            forceDirection += Vector3.right;
+        }
+        rigidbody.AddForce(forceDirection.normalized*10, ForceMode.Acceleration);
+    }
+}
